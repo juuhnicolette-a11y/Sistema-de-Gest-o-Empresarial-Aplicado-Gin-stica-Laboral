21dd758c097eea4e80d165dcb74d59fa3654d6fe
juuhnicolette-a11y <juuhnicolette@gmail.com>
2026-05-14 12:49:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-teste
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def login():
+    return render_template("index.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/cadastros")
+def cadastros():
+    return render_template("cadastros.html")
+
+@app.route("/visualizacao-cadastro")
+def visualizacao_cadastro():
+    return render_template("visualizacao-cadastro.html")
+
+@app.route("/editar-cadastro")
+def editar_cadastro():
+    return render_template("editar-cadastro.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
