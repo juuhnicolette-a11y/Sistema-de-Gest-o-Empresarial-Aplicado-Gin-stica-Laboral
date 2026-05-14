db31d994458417eb7976a621adde5bd724645cee
juuhnicolette-a11y <juuhnicolette@gmail.com>
2026-05-14 16:09:04 +0000

    Flask inicial funcionando

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def login():
+    return render_template("index.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/cadastros")
+def cadastros():
+    return render_template("cadastros.html")
+
+@app.route("/visualizacao-cadastro")
+def visualizacao_cadastro():
+    return render_template("visualizacao-cadastro.html")
+
+@app.route("/editar-cadastro")
+def editar_cadastro():
+    return render_template("editar-cadastro.html")
+
+@app.route("/planejamento")
+def planejamento():
+    return render_template("planejamento.html")
+
+@app.route("/financeiro")
+def financeiro():
+    return render_template("financeiro.html")
+
+@app.route("/relatorios")
+def relatorios():
+    return render_template("relatorios.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
