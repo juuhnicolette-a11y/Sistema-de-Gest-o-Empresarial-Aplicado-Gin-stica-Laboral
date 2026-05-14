32e9fe865f94c2c2fa99076c8a9edda75d9bc78f
juuhnicolette-a11y <juuhnicolette@gmail.com>
2026-05-14 16:31:01 +0000

    Atualização das rotas Flask

--- app.py
+++ app.py
@@ -22,22 +22,73 @@ def visualizacao_cadastro():
 def editar_cadastro():
     return render_template("editar-cadastro.html")
 
-<<<<<<< HEAD
 @app.route("/planejamento")
 def planejamento():
     return render_template("planejamento.html")
+
+@app.route("/exercicios")
+def exercicios():
+    return render_template("exercicios.html")
+
+@app.route("/cadastro-exercicio")
+def cadastro_exercicio():
+    return render_template("cadastro-exercicio.html")
+
+@app.route("/cronogramas")
+def cronogramas():
+    return render_template("cronogramas.html")
+
+@app.route("/cadastro-cronograma")
+def cadastro_cronograma():
+    return render_template("cadastro-cronograma.html")
 
 @app.route("/financeiro")
 def financeiro():
     return render_template("financeiro.html")
 
+@app.route("/contas-pagar")
+def contas_pagar():
+    return render_template("contas-pagar.html")
+
+@app.route("/cadastro-conta-pagar")
+def cadastro_conta_pagar():
+    return render_template("cadastro-conta-pagar.html")
+
+@app.route("/editar-conta-pagar")
+def editar_conta_pagar():
+    return render_template("editar-conta-pagar.html")
+
+@app.route("/contas-receber")
+def contas_receber():
+    return render_template("contas-receber.html")
+
+@app.route("/cadastro-conta-receber")
+def cadastro_conta_receber():
+    return render_template("cadastro-conta-receber.html")
+
+@app.route("/editar-conta-receber")
+def editar_conta_receber():
+    return render_template("editar-conta-receber.html")
+
 @app.route("/relatorios")
 def relatorios():
     return render_template("relatorios.html")
 
+@app.route("/relatorio-cadastros")
+def relatorio_cadastros():
+    return render_template("relatorio-cadastros.html")
+
+@app.route("/relatorio-cronogramas")
+def relatorio_cronogramas():
+    return render_template("relatorio-cronogramas.html")
+
+@app.route("/relatorio-contas-pagar")
+def relatorio_contas_pagar():
+    return render_template("relatorio-contas-pagar.html")
+
+@app.route("/relatorio-contas-receber")
+def relatorio_contas_receber():
+    return render_template("relatorio-contas-receber.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
-=======
-if __name__ == "__main__":
-    app.run(debug=True)
->>>>>>> 57f539c90048fc28c25a199aacff999dda80cb03
